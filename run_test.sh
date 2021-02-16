8f2929d77b59d3133a0e02db51e03fd57a9c7c67
Martin Tysseland <martinty@stud.ntnu.no>
2021-02-16 19:00:51 +0100

    Update run_test for submodule

--- run_test.sh
+++ run_test.sh
@@ -5,12 +5,12 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd)"
 cd $SCRIPTPATH
 
 rm -f feedback.txt
-rm -rf ../automated-testing/src_zip
-mkdir ../automated-testing/src_zip
+rm -rf automated-testing/src_zip
+mkdir automated-testing/src_zip
 
-cp handin.zip ../automated-testing/src_zip
+cp handin.zip automated-testing/src_zip
 
-cd ../automated-testing
+cd automated-testing
 ./setup.sh remove
 ./setup.sh run release
 cp build/feedback.txt $SCRIPTPATH
